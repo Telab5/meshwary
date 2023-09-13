fde92638a780ae424a591e1b85369e16ac0d46e0
Telab5 <87239066+Telab5@users.noreply.github.com>
2023-09-13 20:10:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let  nav = document.getElementById('navScroll');
+window.addEventListener('scroll' ,  function(){
+    if(window.pageYOffset > 100){
+        nav.classList.add('bg-dark' , 'shadow')
+    }else {
+        nav.classList.remove('bg-dark' , 'shadow')
+    }
+});
